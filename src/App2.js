577cadd3991162987d1aa96f93837ea6703d9caf
marynazhurauleva <maryna.zhurauleva@instinctools.com>
2019-10-07 00:41:37 +0300

    Optimize game.

--- src/App2.js
+++ src/App2.js
@@ -8,44 +8,49 @@ export default class App2 extends React.Component {
 
         this.state = {
             isGameStarted: true,
+            sizeOfCell: 10,
             gameFields: Array(10).fill(Array(10).fill(false)),
+            speed: 1000,
         };
     }
 
     toggleGameStarted = () => {
         this.setState({ isGameStarted: !this.state.isGameStarted });
-    }
+    };
 
     changeGameField = (i, j) => {
         const gameFieldsCopy = this.state.gameFields.map(e => e.slice());
         gameFieldsCopy[i][j] = gameFieldsCopy[i][j] ? 0 : 1;
         this.setState({ gameFields: gameFieldsCopy });
-    }
+    };
 
     renderGameCells = () => {
         return this.state.gameFields.reduce((acc, array, i) => {
             array.map((item, j) => {
                 acc.push(
-                    <GameField 
-                        key={`${i}${j}`}
+                    <GameField
+                        key={`${i}--${j}`}
                         isAlive={item}
                         toggleAlive={() => {this.changeGameField(i, j)}}
                     />
                 )
-            })
+            });
             return acc;
         }, [])
-    }
+    };
 
     gameLoop = () => {
-        this.updateFields()
-    }
+        if (this.state.isGameStarted) {
+            this.updateFields()
+            setTimeout(this.gameLoop, this.state.speed);
+        }
+    };
 
     updateFields = () => {
         const fieldsCopy = this.state.gameFields.map(e => e.slice());
 
-        for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 10; j++) {
+        for (let i = 0; i < this.state.sizeOfCell; i++) {
+            for (let j = 0; j < this.state.sizeOfCell; j++) {
             fieldsCopy[i][j] = this.getNewCellState(i, j, this.state.gameFields[i][j]);
             }
         }
@@ -55,7 +60,7 @@ export default class App2 extends React.Component {
 
     getNewCellState = (row, column, isAlive) => {
         let aliveNeighborsCounter = 0;
-    
+
         const neighborsCords = [
             [-1, -1],
             [-1, 0],
@@ -66,12 +71,12 @@ export default class App2 extends React.Component {
             [1, 0],
             [1, 1],
         ];
-    
+
         neighborsCords.forEach(([i, j]) => {
           const fieldsRow = this.state.gameFields[row + i];
           fieldsRow && fieldsRow[column + j] && aliveNeighborsCounter++; // HACH
         });
-    
+
         if(isAlive) {
             return (aliveNeighborsCounter === 2 || aliveNeighborsCounter === 3) ? 1 : 0;
         } else {
@@ -80,19 +85,53 @@ export default class App2 extends React.Component {
     };
 
     startGame = () => {
-        // if (isGameStarted) {
-          // toggleGameStart(true);
+        const { isGameStarted } = this.state;
+        this.setState({ isGameStarted: true });
         this.gameLoop();
-        // }
+    };
+
+    onChangeFieldSize = (event) => {
+        const sizeOfCell = +event.target.value;
+        this.setState({
+            sizeOfCell,
+            gameFields: Array(sizeOfCell).fill(Array(sizeOfCell).fill(false)),
+        });
+    };
+
+    onChangeSpeed = (event) => {
+        const speed = event.target.value;
+        this.setState({ speed });
     };
 
     render() {
+        const { sizeOfCell } = this.state;
+        const fieldStyles = {
+            width : `${sizeOfCell * 30}px`,
+            height: `${sizeOfCell * 30}px`,
+            gridTemplateColumns: `repeat(${sizeOfCell}, 1fr)`,
+            gridTemplateRows: `repeat(${sizeOfCell}, 1fr)`,
+        };
+
         return (
             <>
-                <div className="game">
+                <div style={fieldStyles} className="game">
                     {this.renderGameCells()}
                 </div>
-                <button id="startGame" onClick={this.startGame}>Start the game</button>
+                <div className='gameSettings'>
+                    <div>
+                        <p className='description'>Enter speed</p>
+                        <input type='number' value={this.state.speed} onChange={this.onChangeSpeed}/>
+                    </div>
+                    <div>
+                        <p className='description'>Enter field size</p>
+                        <select onChange={this.onChangeFieldSize} >
+                            <option value={10} default>10</option>
+                            <option value={20}>20</option>
+                            <option value={30}>30</option>
+                        </select>
+                    </div>
+                    <button id='startGame' onClick={this.startGame}>Start the game</button>
+                </div>
             </>
         )
     }
